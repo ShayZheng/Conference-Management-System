5bf482c9c9cb0a6a4eeee22dbdc820408a26f628
Ying Zheng <yzhe0068@student.monash.edu>
2021-04-25 23:02:19 +0800

    upload the initial version of CMS class,User class, Reviewer class and Paper class.

---
+++ .idea/CMS.java
@@ -0,0 +1,23 @@
+//
+// @ date: 25/04/2021
+//
+
+public class CMS {
+
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+
+        Reviewer r = new Reviewer(1, "Shay", "666666", "Reviewer", "IT");
+        System.out.println("-----------Reviewer message------------");
+        System.out.println(r);
+
+        Paper p = new Paper("Distributed application", "03/05/2021", "13/05/2021", "No",
+                "Peter Wong", "IT", "Notknow", "Application");
+
+        System.out.println("\n" + "-----------Paper message------------");
+        System.out.println(p);
+    }
+
+
+}
---
+++ .idea/Model/Paper.java
@@ -0,0 +1,103 @@
+// @ author:Ying Zheng
+// @ date: 25/04/2021
+//
+
+public class Paper {
+    private String Name;
+    private String smDeadline;
+    private String rmDeadline;
+    private String Status;
+    private String Author;
+    private String Keyword;
+    private String Decision;
+    private String conName;
+
+    public Paper(String name, String smDeadline, String rmDeadline, String status, String author, String keyword, String decision, String conName) {
+        Name = name;
+        this.smDeadline = smDeadline;
+        this.rmDeadline = rmDeadline;
+        Status = status;
+        Author = author;
+        Keyword = keyword;
+        Decision = decision;
+        this.conName = conName;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getSmDeadline() {
+        return smDeadline;
+    }
+
+    public void setSmDeadline(String smDeadline) {
+        this.smDeadline = smDeadline;
+    }
+
+    public String getRmDeadline() {
+        return rmDeadline;
+    }
+
+    public void setRmDeadline(String rmDeadline) {
+        this.rmDeadline = rmDeadline;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public String getKeyword() {
+        return Keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        Keyword = keyword;
+    }
+
+    public String getDecision() {
+        return Decision;
+    }
+
+    public void setDecision(String decision) {
+        Decision = decision;
+    }
+
+    public String getConName() {
+        return conName;
+    }
+
+    public void setConName(String conName) {
+        this.conName = conName;
+    }
+
+    @Override
+    public String toString() {
+        return "Paper{" +
+                "Name='" + Name + '\'' +
+                ", smDeadline='" + smDeadline + '\'' +
+                ", rmDeadline='" + rmDeadline + '\'' +
+                ", Status='" + Status + '\'' +
+                ", Author='" + Author + '\'' +
+                ", Keyword='" + Keyword + '\'' +
+                ", Decision='" + Decision + '\'' +
+                ", conName='" + conName + '\'' +
+                '}';
+    }
+}
